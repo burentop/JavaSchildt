016455de714f1d813d7b0f194eda8c9c6384b7d4
Brent Perry <burentoperii@gmail.com>
2016-06-24 16:55:57 -0500

    Add two new constructors to Stack class.

--- Ch6/Stack/src/Stack.java
+++ Ch6/Stack/src/Stack.java
@@ -9,6 +9,26 @@ public class Stack {
 		pushloc = poploc = 0;
 	}
 	
+	// Construct a Stack from a Stack
+	Stack (Stack ob) {
+		pushloc = ob.pushloc;
+		poploc = ob.poploc;
+		s = new char[ob.s.length];
+		
+		// copy elements
+		for (int i = poploc; i < pushloc; i++)
+			s[i] = ob.s[i];
+	}
+	
+	// Construct a Stack with initial values.
+	Stack (char a[]) {
+		pushloc = 0;
+		poploc = 0;
+		s = new char[a.length];
+		
+		for (int i = 0; i < a.length; i++) push(a[i]);
+	}
+	
 	// push a character into the stack
 	void push(char ch) {
 		if (pushloc == s.length) {
