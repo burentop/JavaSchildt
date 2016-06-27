c1114354354942d00d447a00922fbb503428f528
Brent Perry <burentoperii@gmail.com>
2016-06-27 11:10:35 -0500

    Add subclass to Triangle. Subclass of subclass.

--- Ch7/TwoDShape/src/TwoDShape.java
+++ Ch7/TwoDShape/src/TwoDShape.java
@@ -67,31 +67,22 @@ class Triangle extends TwoDShape {
 
 class Shapes {
 	public static void main (String[] args) {
-		Triangle t1 = new Triangle();
-		Triangle t2 = new Triangle("outlined", 8.0, 12.0);
-		Triangle t3 = new Triangle(4.0);
-		
-		t1 = t2;
+		ColorTriangle t1 = new ColorTriangle("Blue", "outlined", 8.0, 12.0);
+		ColorTriangle t2 = new ColorTriangle("Red", "filled", 2.0, 2.0);
 		
 		System.out.println("Info for t1: ");
 		t1.showStyle();
 		t1.showDim();
+		t1.showColor();
 		System.out.println("Area is " + t1.area());
 		
 		System.out.println();
 		
-		System.out.println("Info for t2: ");
+		System.out.println("Infor for t2: ");
 		t2.showStyle();
 		t2.showDim();
+		t2.showColor();
 		System.out.println("Area is " + t2.area());
 		
-		System.out.println();
-		
-		System.out.println("Infor for t3: ");
-		t3.showStyle();
-		t3.showDim();
-		System.out.println("Area is " + t3.area());
-		
-		System.out.println();
 	}
 }
