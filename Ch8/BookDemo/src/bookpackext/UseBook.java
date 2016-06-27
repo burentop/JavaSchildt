bdcd19962f43935fa42a575e608520f82395179e
Brent Perry <burentoperii@gmail.com>
2016-06-27 17:06:23 -0500

    Add subclass to bookpack.Book in different package.

--- Ch8/BookDemo/src/bookpackext/UseBook.java
+++ Ch8/BookDemo/src/bookpackext/UseBook.java
@@ -4,16 +4,23 @@ public class UseBook {
 
 	public static void main (String[] args) {
 		
-		bookpack.Book books[] = new bookpack.Book[5];
+		ExtBook books[] = new ExtBook[5];
 		
-		books[0] = new bookpack.Book("Java: A Beginner's Guide", "Schildt", 2014);
-		books[1] = new bookpack.Book("Java: The Complete Reference", "Schildt", 2014);
-		books[2] = new bookpack.Book("The Art of Java", "Schildt and Holmes", 2003);
-		books[3] = new bookpack.Book("Red Storm Rising", "Clancy", 1986);
-		books[4] = new bookpack.Book("On the Road", "Kerouac", 1955);
+		books[0] = new ExtBook("Java: A Beginner's Guide", "Schildt", 2014, "McGraw-Hill Professional");
+		books[1] = new ExtBook("Java: The Complete Reference", "Schildt", 2014, "McGraw-Hill Professional");
+		books[2] = new ExtBook("The Art of Java", "Schildt and Holmes", 2003, "McGraw-Hill Professional");
+		books[3] = new ExtBook("Red Storm Rising", "Clancy", 1986, "Putnam");
+		books[4] = new ExtBook("On the Road", "Kerouac", 1955, "Viking");
 		
 		for(int i = 0; i < books.length; i++) {
 			books[i].show();
+		}
+		
+		// Find books by author
+		System.out.println("Showing all books by Schildt.");
+		for (int i = 0; i < books.length; i++) {
+			if(books[i].getAuthor() == "Schildt")
+				System.out.println(books[i].getTitle());
 		}
 		
 	}
