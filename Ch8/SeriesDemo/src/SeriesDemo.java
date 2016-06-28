1742cd924fe3cc0fd3c7e10b0729c33e587ec53a
Brent Perry <burentoperii@gmail.com>
2016-06-28 08:27:53 -0500

    Create Series ref variable.

--- Ch8/SeriesDemo/src/SeriesDemo.java
+++ Ch8/SeriesDemo/src/SeriesDemo.java
@@ -3,21 +3,17 @@ public class SeriesDemo {
 
 	public static void main (String[] args) {
 		
-		ByTwos ob = new ByTwos();
-		ByThrees ob2 = new ByThrees();
+		ByTwos twoOb = new ByTwos();
+		ByThrees threeOb = new ByThrees();
+		Series ob;
 		
-		for(int i = 0; i < 5; i++)
-			System.out.println("Next value is " + ob.getNext());
-		
-		System.out.println("\nResetting");
-		ob.reset();
-		for(int i = 0; i < 5; i++)
-			System.out.println("Next value is " + ob.getNext());
-		
-		System.out.println("\nBy Threes");
-		ob2.setStart(3);
-		for(int i = 0; i < 5; i++)
-			System.out.println("Next value is " + ob2.getNext());
+		for (int i = 0; i < 5; i++) {
+			ob = twoOb;
+			System.out.println("Next ByTwos value is " + ob.getNext());
+			
+			ob = threeOb;
+			System.out.println("Next ByThrees value is " + ob.getNext());
+		}
 		
 	}
 }
