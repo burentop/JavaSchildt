19233c8d6f62886286a1f2ce94f45b7210ee815b
Brent Perry <burentoperii@gmail.com>
2016-06-30 15:51:06 -0500

    Initial commit of UseThrowableMethods.

---
+++ Ch9/UseThrowableMethods/src/ExcTest.java
@@ -0,0 +1,13 @@
+
+public class ExcTest {
+
+	static void genException() {
+		int nums[] = new int[4];
+		
+		System.out.println("Before exception is generated.");
+		
+		nums[7] = 10;
+		System.out.println("won't be displayed");
+	}
+	
+}
