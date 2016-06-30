b483a3b7042166fad85eee92392d46119ae247e7
Brent Perry <burentoperii@gmail.com>
2016-06-30 15:42:43 -0500

    Initial commit of Rethrow.

---
+++ Ch9/Rethrow/src/RethrowDemo.java
@@ -0,0 +1,12 @@
+
+public class RethrowDemo {
+	public static void main (String[] args) {
+		
+		try {
+			Rethrow.genException();
+		}
+		catch (ArrayIndexOutOfBoundsException exc) {
+			System.out.println("Fatal error - program terminated.");
+		}
+	}
+}
