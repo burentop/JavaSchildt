7a44ff402f1153ff88cb63b39fb7cb6f5710c4f8
Brent Perry <burentoperii@gmail.com>
2016-06-30 15:59:28 -0500

    Initial commit of FinallyDemo.

---
+++ Ch9/FinallyDemo/src/UseFinally.java
@@ -0,0 +1,33 @@
+
+public class UseFinally {
+
+	public static void genException(int what) {
+		int t;
+		int nums[] = new int[2];
+		
+		System.out.println("Receiving " + what);
+		try {
+			switch(what) {
+			case 0:
+				t = 10 / what;
+				break;
+			case 1:
+				nums[4] = 4;
+				break;
+			case 2:
+				return;
+			}
+		}
+		catch (ArithmeticException exc) {
+			System.out.println("Can't divide by zero.");
+			return;
+		}
+		catch (ArrayIndexOutOfBoundsException exc) {
+			System.out.println("No matching element found.");
+		}
+		finally {
+			System.out.println("Leaving try.");
+		}
+	}
+	
+}
